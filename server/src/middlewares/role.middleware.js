3a5a833cfc55656bc01a5aff8daf4773ecd17114
khushal <khushalsatani009@gmail.com>
2026-03-04 22:22:05 +0530

    feat(middleware): role base access

---
+++ server/src/middlewares/role.middleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import response from '../response/response.js';
+
+const requiredRole = (...allowedRole) => {
+  return (req, res, next) => {
+    try {
+      const token = req.cookies.token;
+
+      if (!token) {
+        return response(res, 401, false, 'Unauthorized: No token');
+      }
+
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      const role = decoded.role;
+
+      if (!allowedRole.includes(role)) {
+        return response(res, 403, false, 'Forbidden: Access denied');
+      }
+
+      req.user = decoded;
+
+      next();
+    } catch (err) {
+      return response(res, 401, false, 'Invalid or expired token');
+    }
+  };
+};
+
+export default requiredRole;
