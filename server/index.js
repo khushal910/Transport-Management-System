b03a349df47e1472bd6a46f357ce9741fb148921
khushal <khushalsatani009@gmail.com>
2026-03-04 22:33:21 +0530

    chore: add morgan and start script

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ import dbConnect from './src/config/dbConnection.js';
 import cors from 'cors';
 import authRouter from './src/routers/auth.route.js';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors()); // For local development
 app.use(cookieParser());
+app.use(morgan('dev'))
 
 
 // API
