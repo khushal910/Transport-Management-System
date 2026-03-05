4dc88a76efe7223cf5dbac8ef2f8b4bcd1fc73bc
khushal <khushalsatani009@gmail.com>
2026-03-05 17:35:47 +0530

    feat(auth): implement user logout

--- server/src/routers/auth.route.js
+++ server/src/routers/auth.route.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRegister from '../controllers/user.registration.js';
 import userLogin from '../controllers/user.login.js';
+import userLogout from '../controllers/user.logout.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', userRegister);
 authRouter.post('/login', userLogin);
+authRouter.post('/logout', userLogout);
 
 export default authRouter;
