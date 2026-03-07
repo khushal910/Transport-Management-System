491a8bbeed2005f420bac4e474a9070b6a33a5c9
khushal <khushalsatani009@gmail.com>
2026-03-07 17:30:28 +0530

    feat(vehicle): create api to get all vehicle

---
+++ server/src/controllers/vehicle/vehicle.getList.js
@@ -0,0 +1,34 @@
+import Vehicle from '../../models/vehicle.schema.js';
+import response from '../../response/response.js';
+
+const getVehicleList = async (req, res) => {
+  try {
+    const userID = req.user.id;
+    if (!userID) {
+      return response(res, 401, false, 'User ID is required');
+    }
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const listOfVehicles = await Vehicle.find({ createdBy: userID })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .lean();
+
+    return response(
+      res,
+      200,
+      true,
+      'Vehicle list fetched successfully',
+      listOfVehicles
+    );
+  } catch (error) {
+    console.log('getVehicleList error:', error);
+    return response(res, 500, false, 'Error fetching vehicle list');
+  }
+};
+
+export default getVehicleList;
--- server/src/routers/vehicle.route.js
+++ server/src/routers/vehicle.route.js
@@ -2,10 +2,12 @@ import {Router} from 'express'
 import requiredRole from '../middlewares/role.middleware.js'
 import vehicleRegister from '../controllers/vehicle/vehicle.register.js'
 import vehicleUpdate from '../controllers/vehicle/vehicle.update.js'
+import getVehicleList from '../controllers/vehicle/vehicle.getList.js'
 
 const vehicleRoute = Router()
 
 vehicleRoute.post('/register', requiredRole('manager'), vehicleRegister)
 vehicleRoute.post('/update/:vehicleId', requiredRole('manager'), vehicleUpdate);
+vehicleRoute.get('/list', requiredRole('manager'), getVehicleList); 
 
 export default vehicleRoute
