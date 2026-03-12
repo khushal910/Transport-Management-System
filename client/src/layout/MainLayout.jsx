dbdb6a41c19c5608f1e72e324dc43b57c5078d03
khushal <khushalsatani009@gmail.com>
2026-03-12 11:28:16 +0530

    feat(client): implement logout with backend logout api

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import authBaseURL from '../api/authBaseURL';
+import { toast } from 'react-toastify';
 
 const MainLayout = () => {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+       await authBaseURL.post(
+        '/logout',
+        { withCredentials: true }
+      );
+      toast.success('Logout successful!');
+      navigate('/login');
+    } catch {
+      toast.error('An error occurred during logout. Please try again.');
+    }
+  };
   return (
     <div className="min-h-screen flex flex-col ">
       <nav className="bg-gray-900 text-white px-6 py-4 flex justify-center items-center gap-6 shadow-md">
@@ -47,12 +61,18 @@ const MainLayout = () => {
         >
           Analytics
         </Link>
+        <button
+          onClick={handleLogout}
+          className="bg-red-700 px-3 py-2 rounded-md text-sm font-medium transition-colors"
+        >
+          Logout
+        </button>
       </nav>
       <main className="flex-1 bg-gray-100 p-6">
         <Outlet />
       </main>
     </div>
   );
-}
+};
 
-export default MainLayout
+export default MainLayout;
